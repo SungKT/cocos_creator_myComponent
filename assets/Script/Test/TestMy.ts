dc73e487f7997b3a089898b734a0069c4fb8ec6d
sungkt <tjvalore@163.com>
2020-08-03 18:51:20 +0800

    first commit

---
+++ assets/Script/Test/TestMy.ts
@@ -0,0 +1,32 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import List from '../Components/List';
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class TestMy extends cc.Component {
+
+    @property(List)
+    list: List = null;
+    data: number[] = [];
+
+    onLoad() {
+        this.data = [];
+        for (let n: number = 0; n < 100; n++) {
+            this.data.push(n);
+        }
+        this.list.numItems = 12;
+    }
+
+    onListRender(item: cc.Node, idx: number) {
+        item.getComponentInChildren(cc.Label).string = this.data[idx] + '333';
+    }
+
+    // update (dt) {}
+}
